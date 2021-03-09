7fb39a370362d8e84e9e464a3bf2192fab425f67
Sz77 <63186233+Sz77@users.noreply.github.com>
2021-03-10 01:45:04 +0200

    add values for AutoAdjusting and tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/movement/AutoDrivingNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/movement/AutoDrivingNew.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import org.firstinspires.ftc.teamcode.java.drivebase.MecanumDrive;
 import org.firstinspires.ftc.teamcode.java.util.Angle;
 import org.firstinspires.ftc.teamcode.java.util.MovementData;
-import org.firstinspires.ftc.teamcode.java.util.PidfController;
+import org.firstinspires.ftc.teamcode.java.util.PositionControl.PositionPidfController;
 import org.firstinspires.ftc.teamcode.java.util.RobotHardware;
 
 /**
@@ -16,9 +16,9 @@ import org.firstinspires.ftc.teamcode.java.util.RobotHardware;
  */
 public class AutoDrivingNew {
 	/**
-	 * {@link PidfController} for the 3 Axes of Movement: X, Y, and θ
+	 * {@link PositionPidfController} for the 3 Axes of Movement: X, Y, and θ
 	 */
-	private final PidfController drivePid, strafePid, turnPid;
+	private final PositionPidfController drivePid, strafePid, turnPid;
 	/**
 	 * A Location Tracking {@link ActiveLocation} Object to Update the Current Location
 	 */
@@ -51,13 +51,13 @@ public class AutoDrivingNew {
 
 	/**
 	 * The Basic Constructor to Create a Basic Instance of AutoDriving
-	 * @param drivePid the forward and reverse {@link PidfController}
-	 * @param strafePid the left and right {@link PidfController}
-	 * @param turnPid the rotation {@link PidfController}
+	 * @param drivePid the forward and reverse {@link PositionPidfController}
+	 * @param strafePid the left and right {@link PositionPidfController}
+	 * @param turnPid the rotation {@link PositionPidfController}
 	 * @param robot the {@link RobotHardware} initiated with a HardwareMap
 	 */
-	public AutoDrivingNew(PidfController drivePid, PidfController strafePid,
-	                      PidfController turnPid, RobotHardware robot) {
+	public AutoDrivingNew(PositionPidfController drivePid, PositionPidfController strafePid,
+	                      PositionPidfController turnPid, RobotHardware robot) {
 		// Connects PID Controllers with tuned variants
 		this.drivePid   = drivePid;
 		this.strafePid  = strafePid;
@@ -82,9 +82,9 @@ public class AutoDrivingNew {
 
 	/**
 	 * The Basic Constructor to Create a Basic Instance of AutoDriving
-	 * @param drivePid the forward and reverse {@link PidfController}
-	 * @param strafePid the left and right {@link PidfController}
-	 * @param turnPid the rotation {@link PidfController}
+	 * @param drivePid the forward and reverse {@link PositionPidfController}
+	 * @param strafePid the left and right {@link PositionPidfController}
+	 * @param turnPid the rotation {@link PositionPidfController}
 	 * @param frontRightMotor the Front Right {@link DcMotor} on the Robot
 	 * @param frontLeftMotor the Front Left {@link DcMotor} on the Robot
 	 * @param backRightMotor the Back Right {@link DcMotor} on the Robot
@@ -93,8 +93,8 @@ public class AutoDrivingNew {
 	 * @param yAxisEncoder a {@link DcMotor} which is linked to the Y Axis Encoder
 	 * @param gyroscope the imu (aka gyroscope) to determine the angle / orientation of the Robot
 	 */
-	public AutoDrivingNew(PidfController drivePid, PidfController strafePid,
-	                      PidfController turnPid, DcMotor frontRightMotor, DcMotor frontLeftMotor,
+	public AutoDrivingNew(PositionPidfController drivePid, PositionPidfController strafePid,
+	                      PositionPidfController turnPid, DcMotor frontRightMotor, DcMotor frontLeftMotor,
 	                      DcMotor backRightMotor, DcMotor backLeftMotor, DcMotor xAxisEncoder,
 	                      DcMotor yAxisEncoder, BNO055IMU gyroscope) {
 		// Connects PID Controllers with tuned variants
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/tests/AutoDrivingTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/java/tests/AutoDrivingTest.java
@@ -3,10 +3,12 @@ package org.firstinspires.ftc.teamcode.java.tests;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.java.movement.AutoDriving;
+import org.firstinspires.ftc.teamcode.java.movement.*;
 import org.firstinspires.ftc.teamcode.java.util.*;
 
 
@@ -20,19 +22,18 @@ public class AutoDrivingTest extends LinearOpMode {
     // Declare OpMode members.
     RobotHardware robot = new RobotHardware();
     //Hardware robot = new Hardware();
-    private DcMotor frontRightMotor;
-    private DcMotor frontLeftMotor;
-    private DcMotor backLeftMotor;
-    private DcMotor backRightMotor;
+//    private DcMotor frontRightMotor;
+//    private DcMotor frontLeftMotor;
+//    private DcMotor backLeftMotor;
+//    private DcMotor backRightMotor;
     private BNO055IMU imu;
-//    private ActiveLocation AL;
-//    private Thread locationThread;
-//    private PathFinder PF;
+    private CRServo pl;
+    private CRServo pn;
+    private ActiveLocation AL;
+    private Thread locationThread;
+    //    private PathFinder PF;
 //    private Thread pathThread;
-    private AutoDriving autoDriving;
-    private PidfController PIDFDrive;
-    private PidfController PIDFStrafe;
-    private PidfController PIDFTurn;
+    private AutoAdjusting autoAdjusting;
 
     private boolean location;
 
@@ -41,46 +42,57 @@ public class AutoDrivingTest extends LinearOpMode {
     public void runOpMode() {
         location = true;
         robot.init(hardwareMap);
-
+        pl = robot.lShooter;
         imu = robot.imu;
+//
+//        frontLeftMotor = robot.frontLeftMotor;
+//        frontRightMotor = robot.frontRightMotor;
+//        backRightMotor = robot.backRightMotor;
+//        backLeftMotor = robot.backLeftMotor;
 
-        frontLeftMotor = robot.frontLeftMotor;
-        frontRightMotor = robot.frontRightMotor;
-        backRightMotor = robot.backRightMotor;
-        backLeftMotor = robot.backLeftMotor;
 
-
-        /*AL = new ActiveLocation(robot);
+        AL = new ActiveLocation(robot);
         locationThread = new Thread(AL);
         locationThread.start();
+        autoAdjusting= new AutoAdjusting(robot,AL,Side.RED,this);
 
-        PF = new PathFinder(AL);
-        pathThread = new Thread(PF);
-        pathThread.start();
+//        PF = new PathFinder(AL);
+//        pathThread = new Thread(PF);
+//        pathThread.start();
+//
+//         */
+//        //12096
+//        PIDFDrive = PositionPidfConstants.USDrive;//new PIDFController(0.0011844/*96004999*/, 0.0000000000000, 0.00150719/*423*/, 0); //003,000001,003705
+//        //PIDFDrive = new PIDFController(0.00, 0.000000, 0.00, 0);
+//        PIDFStrafe = PositionPidfConstants.USStrafe;//new PIDFController(0.001705, 0.000000000000001, 0.005705, 0);
+//        PIDFTurn = PositionPidfConstants.USTurn; //new PIDFController(0.35, 0.000000000, 0.395, 0); //38
+//        //PIDFTurn = new PIDFController(0, 0, 0, 0); //38
+//
+//        autoDriving = new AutoDriving(PIDFDrive, PIDFStrafe, PIDFTurn, robot);
 
-         */
-        //12096
-        PIDFDrive = PidfConstants.USDrive;//new PIDFController(0.0011844/*96004999*/, 0.0000000000000, 0.00150719/*423*/, 0); //003,000001,003705
-        //PIDFDrive = new PIDFController(0.00, 0.000000, 0.00, 0);
-        PIDFStrafe = PidfConstants.USStrafe;//new PIDFController(0.001705, 0.000000000000001, 0.005705, 0);
-        PIDFTurn = PidfConstants.USTurn; //new PIDFController(0.35, 0.000000000, 0.395, 0); //38
-        //PIDFTurn = new PIDFController(0, 0, 0, 0); //38
 
-        autoDriving = new AutoDriving(PIDFDrive, PIDFStrafe, PIDFTurn, robot);
-
-
-        telemetry.addData("Status", "Initialized");
+        telemetry.addData("Status", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa");
         telemetry.update();
-
 
         waitForStart();
         runtime.reset();
         int movement = 0;
 
-        //while (opModeIsActive()) {
-        // run until the end of the match (driver presses STOP)
-        try {
 
+	    telemetry.addData("Test", "1");
+	    telemetry.update();
+	    //while (opModeIsActive()) {
+        // run until the end of the match (driver presses STOP)
+//        try {
+            movement = 1;
+            while (opModeIsActive()) {
+            	if (movement == 1) {
+		            telemetry.addData("Test", "qwertyuytresdcvbhjuytrdbfghtytfvcbvbmgjhfgbfng");
+		            telemetry.update();
+		            sleep(100);
+	            }
+                movement = 2;
+            }
             //AL.setStartPosition(0, 0);
             //PF.setDestination(600,600);
             /*
@@ -96,87 +108,73 @@ public class AutoDrivingTest extends LinearOpMode {
             //telemetry.addData("Path: ", PF.getEncoderPath());
             //telemetry.update();
 
-            if (opModeIsActive() && !isStopRequested()) {
-                //frontLeftMotor.setPower(.29);
-                autoDriving.setStartLocation(0,0,0);
-                //autoDriving.setStartLocation(600,00,0);
-                autoDriving.stopAt(MovementData.withDegrees(0,0,90),.3);
-                movement +=1; //??? Might cause issue
-                //autoDriving.driveX(600);
-//                autoDriving.stopAt(MovementData.withDegrees(-600, 00,0), .3);
-//                sleep(1000);
-//                autoDriving.stopAt(MovementData.withDegrees(600, 00,0), .3);
-//                sleep(1000);
-//                autoDriving.stopAt(MovementData.withDegrees(0, 00,0), .3);
-//                movement = 2;
-                //autoDriving.turnOff();
-
-//                if (movement == 0) {
-//                    location = true;
-//                    //String report = autoDriving.stopAt(MovementData.withDegrees(00, 00, ), .3);
-//                    stat = autoDriving.stopAt(MovementData.withDegrees(00, 600,0 ), .3);
-//                    //String report = autoDriving.errorReport(MovementData.withDegrees(600, 0, 0));
-//                    //telemetry.addData("Error Report", report);
-//                    //telemetry.update();
-//                }else if (movement == 1){
-//                    //telemetry.speak("In");
-//                    stat = autoDriving.stopAt(MovementData.withDegrees(00, 00,00 ), .3);
-//                }else{
-//                    autoDriving.turnOff();
-//                }
-//                telemetry.speak(""+movement+" "+stat);
-//                if (stat){
-//                    movement+=1;
-//                    sleep(1000);
-//                    stat = false;
-//                }
-//                telemetry.addData("Report ", autoDriving.errorReport(0,0,0));
-//                telemetry.update();
-
-
-
-                //String report = autoDriving.errorReport(new MovementData(600, 600, 90));
-                //telemetry.addData("Error Report", report);
-                //telemetry.speak("Hello" + stat);
-            /*telemetry.addData("Angle", PIDF.getAngleErrorDegrees());
+            //if (opModeIsActive() && !isStopRequested()) //frontLeftMotor.setPower(.29);
+            // autoDriving.setStartLocation(0,0,0);
+            //autoDriving.setStartLocation(600,00,0);
+            // autoDriving.stopAt(MovementData.withDegrees(0,0,90),.3);
+            // movement +=1; //??? Might cause issue
+            //autoDriving.driveX(600);
+            //                autoDriving.stopAt(MovementData.withDegrees(-600, 00,0), .3);
+            //                sleep(1000);
+            //                autoDriving.stopAt(MovementData.withDegrees(600, 00,0), .3);
+            //                sleep(1000);
+            //                autoDriving.stopAt(MovementData.withDegrees(0, 00,0), .3);
+            //                movement = 2;
+            //autoDriving.turnOff();
+            //                if (movement == 0) {
+            //                    location = true;
+            //                    //String report = autoDriving.stopAt(MovementData.withDegrees(00, 00, ), .3);
+            //                    stat = autoDriving.stopAt(MovementData.withDegrees(00, 600,0 ), .3);
+            //                    //String report = autoDriving.errorReport(MovementData.withDegrees(600, 0, 0));
+            //                    //telemetry.addData("Error Report", report);
+            //                    //telemetry.update();
+            //                }else if (movement == 1){
+            //                    //telemetry.speak("In");
+            //                    stat = autoDriving.stopAt(MovementData.withDegrees(00, 00,00 ), .3);
+            //                }else{
+            //                    autoDriving.turnOff();
+            //                }
+            //                telemetry.speak(""+movement+" "+stat);
+            //                if (stat){
+            //                    movement+=1;
+            //                    sleep(1000);
+            //                    stat = false;
+            //                }
+            //                telemetry.addData("Report ", autoDriving.errorReport(0,0,0));
+            //                telemetry.update();
+            //String report = autoDriving.errorReport(new MovementData(600, 600, 90));
+            //telemetry.addData("Error Report", report);
+            //telemetry.speak("Hello" + stat);
+ /*telemetry.addData("Angle", PIDF.getAngleErrorDegrees());
             telemetry.addData("FL", frontLeftMotor.getPower());
             telemetry.addData("FR", frontRightMotor.getPower());
             telemetry.addData("BL", backLeftMotor.getPower());
             telemetry.addData("BR", backRightMotor.getPower());
 
              */
-                //telemetry.update();
-                //sleep(200);
-
-                if (runtime.milliseconds() >= 29000 || movement>1) {
-                    location = false;
-                    frontRightMotor.setPower(0);
-                    frontLeftMotor.setPower(0);
-                    backRightMotor.setPower(0);
-                    backLeftMotor.setPower(0);
-                    //String report = autoDriving.errorReport(MovementData.withDegrees(0, 600, 0));
-                    //telemetry.addData("Error Report", report);
-                    //frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    //backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    telemetry.speak("Done");
-                    telemetry.update();
-                    requestOpModeStop();
-                    //AL.Stop();
-                    //PF.stop();
-
-                }
-
-            }
+            //telemetry.update();
+            //sleep(200);
+            //if (runtime.milliseconds() >= 29000 || movement>1) // location = false;
+            //                    frontRightMotor.setPower(0);
+            //                    frontLeftMotor.setPower(0);
+            //                    backRightMotor.setPower(0);
+            //                    backLeftMotor.setPower(0);
+            //String report = autoDriving.errorReport(MovementData.withDegrees(0, 600, 0));
+            //telemetry.addData("Error Report", report);
+            //frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            //backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             //AL.Stop();
             //PF.stop();
-        } catch (Exception e) {
-            telemetry.addData("error:", e.getStackTrace());
-            telemetry.addData("bob", location);
-            telemetry.update();
-            //AL.Stop();
+
             //PF.stop();
-            sleep(10000);
-        }
+//        } catch (Exception e) {
+//            telemetry.addData("error:", e.getStackTrace());
+//            telemetry.addData("1",movement);
+//            telemetry.update();
+//            //AL.Stop();
+//            //PF.stop();
+//            sleep(10000);
+//        }
         //}
     }
 
